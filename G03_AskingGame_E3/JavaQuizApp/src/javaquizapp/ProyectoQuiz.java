1ae05c2aaab8924bdf733f454bde741280435d05
shirleyascencio <shirley.ascencio@vallegrande.edu.pe>
2022-11-22 02:19:34 -0500

    Entrega 3

---
+++ G03_AskingGame_E3/JavaQuizApp/src/javaquizapp/ProyectoQuiz.java
@@ -0,0 +1,19 @@
+
+package javaquizapp;
+
+
+public class ProyectoQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuPrincipal().setVisible(true);
+            }
+        });
+    }
+    
+}
